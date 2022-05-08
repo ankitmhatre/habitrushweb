578349eed8a5fc91047efac82bd9a7a24ffc317b
Ankit <ankitmhatre.mhatre9@gmail.com>
2022-05-08 06:13:00 +0530

    habit create imcpl

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,13 @@ const FieldValue = admin.firestore.FieldValue;
 admin.initializeApp();
 
 const { PubSub } = require("@google-cloud/pubsub");
+const moment = require("moment-timezone");
 const pubsubClient = new PubSub({ projectId: process.env.GCLOUD_PROJECT });
 
 exports.firePubsub = functions.https.onRequest(async (request, response) => {
   const publisher = pubsubClient.topic("topic1");
   const data = {
-    name: "FunctionsTrigger",
+    trigger: "functions",
   };
   const dataBuffer = Buffer.from(JSON.stringify(data));
   await publisher.publish(dataBuffer);
@@ -17,77 +18,93 @@ exports.firePubsub = functions.https.onRequest(async (request, response) => {
   response.status(200).send("Fired PubSub");
 });
 
-exports.myFunction = functions.pubsub
+exports.createTasksAllHabitsAllUsers = functions.pubsub
   .topic("topic1")
   .onPublish(async (message, context) => {
-    var allUsers = await admin.firestore().collectionGroup("users").get();
-
-    allUsers.forEach(async (doc) => {
-      // console.log(doc.id, '=>',  doc.data())
-      var allHabits = await admin
+    if (
+      message.json.trigger === "cronjob" ||
+      message.json.trigger === "functions"
+    ) {
+      var allUsers = await admin
         .firestore()
-        .collection("users")
-        .doc(doc.id)
-        .collection("habits")
+        .collectionGroup("users")
+        .where("active", "==", true)
         .get();
 
-      allHabits.forEach(async (habit) => {
-        // console.log(habit.id, '=>',  habit.data())
+      if (allUsers.empty) {
+        console.log("No matching users.");
+        return null;
+      }
 
-        var thiHabit = await admin
+      allUsers.forEach(async (user) => {
+        // console.log(doc.id, '=>',  doc.data())
+        var allHabits = await admin
           .firestore()
           .collection("users")
-          .doc(doc.id)
+          .doc(user.id)
           .collection("habits")
-          .doc(habit.id)
-          .update({
-            lastTriggerAt: admin.firestore.FieldValue.serverTimestamp(),
-          });
+          .where("archived", "==", false)
+          .where("active", "==", true)
+          .get();
+
+        if (allHabits.empty) {
+          console.log("No matching users.");
+          return null;
+        }
+
+        allHabits.forEach(async (habit) => {
+          // console.log(habit.id, '=>',  habit.data())
+
+          var updatedHabit = await admin
+            .firestore()
+            .collection("users")
+            .doc(doc.id)
+            .collection("habits")
+            .where("archived", "==", false)
+            .where("active", "==", true)
+            .update({
+              lastTriggerAt: admin.firestore.FieldValue.serverTimestamp(),
+            });
+        });
       });
-      // var docs =
-    });
+    }
+    return null;
   });
 
-// exports.webhook = functions.pubsub.schedule('0 23 * * *')
-// .timeZone('Asia/Kolkata') // Users can choose timezone - default is America/Los_Angeles
-// .onRun((context) => {
-// console.log(context)
-// });
+/////////////////////////////////////////
+// exports.onHabitsTasksUpdated = functions.firestore
+//   .document("users/{uid}/habits/{habitId}/tasks/{taskId}")
+//   .onUpdate(async (change, context) => {
+//     var uid = context.params.uid;
+//     var userDocument = await admin
+//       .firestore()
+//       .doc("users/" + uid)
+//       .get();
+//     var token = userDocument.data().token;
 
-/**
- * WeLcome email
- */
+//     var message = {
+//       tokens: [token],
+//       notification: {
+//         title: "Tasks got updated for a new habit",
+//         // body: JSON.stringify(change.after.data().tasks),
+//       },
+//       data: {
+//         tasks: JSON.stringify(change.after.data()),
+//       },
+//     };
 
-exports.onHabitsTasksUpdated = functions.firestore
-  .document("users/{uid}/habits/{habitId}/tasks/{taskId}")
-  .onWrite(async (change, context) => {
-    var uid = context.params.uid;
-    var userDocument = await admin
-      .firestore()
-      .doc("users/" + uid)
-      .get();
-    var token = userDocument.data().token;
+//     return admin
+//       .messaging()
+//       .sendMulticast(message)
+//       .then((res) => console.log("then", res))
+//       .catch((err) => console.log("catch", err));
+//   });
 
-    var message = {
-      tokens: [token],
-      notification: {
-        title: "Tasks got updated for a new habit",
-        // body: JSON.stringify(change.after.data().tasks),
-      },
-      data: {
-        tasks: JSON.stringify(change.after.data()),
-      },
-    };
-
-    return admin
-      .messaging()
-      .sendMulticast(message)
-      .then((res) => console.log("then", res))
-      .catch((err) => console.log("catch", err));
-  });
+/////////////////////////////////////////
 
 exports.onHabitsCreatedForUser = functions.firestore
   .document("users/{uid}/habits/{habitId}")
+
   .onWrite(async (change, context) => {
     if (change.after.data() === undefined) {
       console.log("habit", "habit deleted");
@@ -98,91 +115,298 @@ exports.onHabitsCreatedForUser = functions.firestore
     //      //habit was created
     // }
     else {
-      console.log("habit", " updated");
-      //Habit was updated/created
-
       var uid = context.params.uid;
       var habitId = context.params.habitId;
       var habitDocument = change.after.data();
-      var startDate = habitDocument.habitStartDate.toDate();
-      var currentDateTime = new Date();
-      var shouldCreateReminders = false;
+      var isHabitCreated = false;
 
-      switch (habitDocument.habitReminderFrequency) {
-        case "Everyday":
-          shouldCreateReminders = true;
-          break;
-        case "Every x days":
-          var everyxDays = habitDocument.habitReminderFrequencyDays[0];
-          break;
-        case "Weekly":
-          habitDocument.habitReminderFrequencyDays.map((k) => {
-            var day = 0;
+      var startDateTimeOffsetInDays = 0;
+      var endDateTimeOffsetInDays = 0;
+      var createReminderOnDatesWhenCreated = [];
 
-            switch (k) {
-              case "Su":
-                day = 0;
-                break;
-              case "Mo":
-                day = 1;
-                break;
-              case "Tu":
-                day = 2;
-                break;
-              case "We":
-                day = 3;
-                break;
-              case "Th":
-                day = 4;
-                break;
-              case "Fr":
-                day = 5;
-                break;
-              case "Sa":
-                day = 6;
-                break;
-            }
+      /**
+       * GET CURRENT DATE TIME IN LOCAL OF WHERE THE HABIT IS CREATED/UPDATED
+       */
+      var currentDateTimeInLocal = moment(new Date());
+      var startDate = moment(new Date());
+      var endDate = moment(new Date());
+      var habitStartDate = moment(habitDocument.habitStartDate.toDate());
 
-            if (currentDateTime.getDay() + 1 == day) {
-              shouldCreateReminders = true;
-            }else if(currentDateTime.getDay() ==6 && day==0){
-              shouldCreateReminders = true;
-            }
-          });
+      lastCompletedDate =
+        habitDocument.habitLastCompletedAtDate !== 0
+          ? moment(habitDocument.habitLastCompletedAtDate.toDate())
+          : 0;
 
-          break;
-          case "Monthly" : 
-          habitDocument.habitReminderFrequencyDays.map((k)=>{
-          if(k==currentDateTime.getDate()+1){
-            shouldCreateReminders = true;
-          }
-          })
-        
-          break;
-      }
+      if (habitDocument.habitCreatedTimeOffset.n) {
+        currentDateTimeInLocal.subtract(
+          habitDocument.habitCreatedTimeOffset.m,
+          "m"
+        );
 
+        startDate.subtract(habitDocument.habitCreatedTimeOffset.m, "m");
+        endDate.subtract(habitDocument.habitCreatedTimeOffset.m, "m");
 
-      if(shouldCreateReminders){
-        console.log("shouldCreateReminders")
-        var updatedTimings = habitDocument.habitRemindAt.map((e) => {
-          var newHours = e.split(":");
-          var remindDate = startDate;
-          remindDate = remindDate.setHours(
-            parseInt(newHours[0]),
-            parseInt(newHours[1])
+        habitStartDate.subtract(habitDocument.habitCreatedTimeOffset.m, "m");
+
+        if (lastCompletedDate !== 0) {
+          lastCompletedDate.subtract(
+            habitDocument.habitCreatedTimeOffset.m,
+            "m"
           );
-  
-          var remindDateInDateFormat = new Date(remindDate);
-  
-          return {
-            completeStatus: false,
-            createdAt: new Date(),
-            remindAt: remindDateInDateFormat,
-          };
-        });
+        }
+      } else {
+        currentDateTimeInLocal.add(habitDocument.habitCreatedTimeOffset.m, "m");
+        startDate.add(habitDocument.habitCreatedTimeOffset.m, "m");
+        endDate.add(habitDocument.habitCreatedTimeOffset.m, "m");
+        habitStartDate.add(habitDocument.habitCreatedTimeOffset.m, "m");
+
+        if (lastCompletedDate !== 0) {
+          lastCompletedDate.add(habitDocument.habitCreatedTimeOffset.m, "m");
+        }
       }
+
+      //////////////////////////////////////////////
+
+      //   console.log("startDateee", startDate.startOf("day"));
+
+      //   console.log("habit Date", habitStartDate.startOf("day"));
+      //   console.log("end  Date", endDate.endOf("day"));
+      //   console.log(
+      //     "startDateDateDIFFF",
+      //     startDate.date() - habitStartDate.date()
+      //   );
+
+      // console.log(" startan nd habit date ", startDate.startOf("day").isSameOrAfter(habitStartDate.startOf("day")))
+      // if (startDate.startOf("day").isAfter(habitStartDate.startOf("day"))) {
+      //   console.log("HABIT STARTS LATER DAYS");
+      //   return "HABIT STARTS LATER DAYS";
+      // }
+
+      //   console.log("HABIT STARTS NOWWW");
+
+      //   //       console.log("falls under start date : LETS CONTINUE")
+
+      /////////////////////////////////////////////////////////////////////////
+
+      /**
+       *  CHECKING HERE IF HABIT CREATED OT UPDATED
+       */
+      if (change.before.data() === undefined) {
+        //HABIT CREATED
+        isHabitCreated = true;
+
+        endDateTimeOffsetInDays = 2;
+        // endDate.set({ h: 0, m: 0, s: 0 });
+        endDate.add(endDateTimeOffsetInDays, "d").endOf("d");
+        console.log("endDate", endDate);
+
+        var tempHabitStartDate = habitStartDate
+          .clone()
+          .set({ h: 0, m: 0, s: 0 });
+        console.log("habitStartDate", habitStartDate);
+        console.log("endDate", endDate);
+        console.log(
+          "days difference",
+          endDate.startOf().diff(tempHabitStartDate.startOf(), "d")
+        );
+
+        console.log("///////////////////////////////////////////////");
+
+        for (
+          var m = tempHabitStartDate;
+          endDate - tempHabitStartDate > 0;
+          tempHabitStartDate.add(1, "days")
+        ) {
+          console.log("createReminderOnDatesWhenCreated",m)
+          createReminderOnDatesWhenCreated.push(m.clone());
+        }
+        console.log("///////////////////////////////////////////////");
+
+        console.log("habitStartDate", habitStartDate);
+        console.log("endDate", endDate);
+
+        if (createReminderOnDatesWhenCreated.length == 0) {
+          console.log(
+            "HABIT CREATED, HABIT START DATE IS BEYOND OUR SCHEDULING SCOPE, CRON JOB WILL TAKE CARE"
+          );
+          return;
+        }
+      } else {
+        startDate.set({ h: 0, m: 0, s: 0 });
+        startDate.add(2, "d");
+        //HABIT UPDATED
+        isHabitCreated = false;
+        startDateTimeOffsetInDays = 2;
+        endDateTimeOffsetInDays = 2;
+        // endDate.set({ h: 0, m: 0, s: 0 });
+        endDate = startDate.clone().endOf("d");
+
+        console.log("=======habitStartDate", habitStartDate);
+        console.log("======startdate", startDate);
+        console.log("=======endDate", endDate);
+
+        if (
+          habitStartDate.isSameOrBefore(startDate, "d") &&
+          habitStartDate.isSameOrBefore(endDate, "d")
+        ) {
+          createReminderOnDatesWhenCreated.push(startDate.clone());
+        }
+        if (createReminderOnDatesWhenCreated.length == 0) {
+          console.log(
+            "HABIT UPDATED, HABIT START DATE IS BEYOND OUR SCHEDULING SCOPE, NEXT CRON JOB WILL TAKE CARE"
+          );
+          return;
+        }
+      }
+
+
+      var createdReminders = []
+      console.log("habitDocument.habitReminderFrequency", habitDocument.habitReminderFrequency)
+console.log("habitDocument.habitReminderFrequencyDays", habitDocument.habitReminderFrequencyDays)
+
+      createReminderOnDatesWhenCreated.map(createReminderDate=>{
+
+console.log("createReminderDate", createReminderDate)
+
+
+
+
+        var shouldCreateReminders = false;
+
+
+
+        switch (habitDocument.habitReminderFrequency) {
+          case "Everyday":
+            shouldCreateReminders = true;
+            break;
+          case "Every x days":
+            if (habitDocument.habitLastCompletedAtDate === 0) {
+              //new habit
+              shouldCreateReminders = true;
+            } else {
+              var everyXDays = habitDocument.habitReminderFrequencyDays[0];
+              var differenceInDays = createReminderDate.diff(
+                lastCompletedDate,
+                "days"
+              );
+              shouldCreateReminders = differenceInDays >= everyXDays;
+  
+              console.log("lastCompletedDate", lastCompletedDate);
+             // console.log("currentDateTime", currentDateTimeInLocal);
+              console.log(differenceInDays);
+              console.log(shouldCreateReminders);
+            }
+            break;
+          case "Weekly":
+            habitDocument.habitReminderFrequencyDays.map((k) => {
+              switch (k) {
+                case "Su":
+                  if (createReminderDate.day() == 0) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+                case "Mo":
+                  if (createReminderDate.day() == 1) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+                case "Tu":
+                  if (createReminderDate.day() == 2) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+                case "We":
+                  if (createReminderDate.day() == 3) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+                case "Th":
+                  if (createReminderDate.day() == 4) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+                case "Fr":
+                  if (createReminderDate.day() == 5) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+                case "Sa":
+                  if (createReminderDate.day() == 6) {
+                    shouldCreateReminders = true;
+                  }
+                  break;
+              }
+  
+
+            });
+  
+            break;
+          case "Monthly":
+            console.log("startDate", createReminderDate.date());
+            console.log("Monthly", habitDocument.habitReminderFrequencyDays);
+            habitDocument.habitReminderFrequencyDays.map((k) => {
+              if (k == createReminderDate.date()) {
+                shouldCreateReminders = true;
+              }
+            });
+  
+            break;
+        }
+
+        console.log("shouldCreateReminders", shouldCreateReminders, createReminderDate);
+
+
+if(shouldCreateReminders){
+  createdReminders.push(createReminderDate.clone())
+}
+
+
+      })
+
+
+
+      if(createdReminders.length <= 0){
+        console.log("NO VALID DATES")
+        return 
+      }else{
+
+        createdReminders.map(reminderDate =>{
+
+
+
+
+console.log("reminderDate", reminderDate)
+
+
+        })
+
+
+        // startDate.set({ h: 0, m: 0, s: 0 });
+        // var reminders = [];
+  
+        // habitDocument.habitRemindAt.forEach((e) => {
+        //   var newHours = e.split(":");
+        //   console.log("newHours", newHours);
+        //   var totalMinutes = e[0] * 60 + e[1];
+  
+        //   startDate.add(totalMinutes, "m");
+        //   reminders.push(startDate);
+        //   console.log("remindAt Time", startDate);
+        //   startDate.subtract(totalMinutes, "m");
+        // });
+      }
+
+
+  
+
+//      console.log(reminders);
     }
-  return
+    //   return {
+    //     completeStatus: false,
+    //     createdAt: new Date(),
+    //     remindAt: remindDateInDateFormat,
+    //   };
+    return;
   });
 
 /////////////////////////////////////////
